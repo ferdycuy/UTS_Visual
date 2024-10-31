4723e085b0775bb22851d5c70418629a0ca9e63f
ferdycuy <anaferdy01@gmail.com>
2024-10-31 22:38:36 +0700

    first

---
+++ pratikum1&2/src/pratikum1/pkg2/Latihan1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pratikum1.pkg2;
+
+/**
+ *
+ * @author ferdy
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+    System.out.println("7+2");
+    System.out.println(7+2);
+    }
+}
---
+++ pratikum1&2/src/pratikum1/pkg2/Variabel_TipeData.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pratikum1.pkg2;
+
+/**
+ *
+ * @author ferdy
+ */
+public class Variabel_TipeData {
+    public static void main(String[] args) {
+int var1 = 56;
+double var2 = 0.543;
+boolean var3 = true;
+char var4 = 'A';
+String var5 = "Belajar Java";
+System.out.println(var1);
+System.out.println(var2);
+System.out.println(var3);
+System.out.println(var4);
+System.out.println(var5);
+}
+}
